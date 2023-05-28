ed5b99457acf57b5e1f0fdd987f1ca2685c96627
Devrathi22 <123661082+Devrathi22@users.noreply.github.com>
2023-05-28 10:16:06 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+// var popup = document.getElementsByClassName("bigdiv")[0];
+// document.addEventListener('mouseout', mouseEvent)
+
+// // unction appearpopup(e){
+// //     if (!e.toElement && !e.relatedTarget) {
+// //         popup.classList.add('visible'); 
+// //     }
+// // }
+//  const mouseEvent = e => {
+//         if (!e.toElement && !e.relatedTarget) {
+//          document.removeEventListener('mouseout', mouseEvent);
+//  console.log="Hello";
+//         popup.classList.add('visible');
+//      }
+//  };
+//  function mouseEvdocument.removeEventListener('mouseout', mouseEvent);
+//     // console.log="Hello";   
+//     // document.getElementsByClassName("bigdiv").className="bigdiv visible";
+// // }
+
+// console.log(popup.className)
+
+// let name=popup.className;
+// console.log(let);
+
+
